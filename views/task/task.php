ae6406608eb1807af0547ae5ef3c9ad5ec31789a
Andrey <voprosi-1@yandex.ru>
2019-06-01 02:46:48 +0300

    Less3: Детальный вид задачи пока в статике.

--- views/task/task.php
+++ views/task/task.php
@@ -1,11 +1,12 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>static:Купить собаке корм</h1>            
+            <h1>Купить корм собаке</h1>            
             <p>
             static:Купить собаке корм не на рынке, а в магазе            
             </p>
-            <a href="http://yii2base/index.php?r=task/tasks">Назад</a>
+            <a href="http://yii2base/index.php?r=tasks">Назад</a>
         </div>
     </div>
 </div>
+
--- views/tasks/index.php
+++ views/tasks/index.php
@@ -1,42 +1,43 @@
 <?php
 
+use app\models\tables\Tasks;
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\widgets\ListView;
+use yii\data\ActiveDataProvider;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\tables\SearchT */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Tasks';
+$this->title = 'Все задачи';
 $this->params['breadcrumbs'][] = $this->title;
+
+$dataProvider = new ActiveDataProvider([
+    'query' => Tasks::find()->orderBy('creator_id'),
+    'pagination' => [
+        'pageSize' => 2,
+    ],
+]);
 ?>
+
+
 <div class="tasks-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Tasks', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Добавить задачу', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
-            'name',
-            'description',
-            'creator_id',
-            'responsible_id',
-            //'deadline',
-            //'status_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
+ <?= ListView::widget([
+    'dataProvider' => $dataProvider,
+    //'filterModel' => $searchModel,
+    'itemView' => '_list',
+    ]);
+?>
 
 </div>
